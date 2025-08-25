c9b960dfba385131ef97a7718972a552086d2ac2
hoangnguyen147 <huyhoangnguyen147z@gmail.com>
2025-08-25 18:03:53 +0700

    isomorphic string

---
+++ leetcode/205.isomorphic_string/solution/solution.go
@@ -0,0 +1,23 @@
+package solution
+
+func isIsomorphic(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+    m1, m2 := make(map[byte]byte), make(map[byte]byte)
+    for i := 0; i < len(s); i++ {
+        if _, ok := m1[s[i]]; !ok {
+            m1[s[i]] = t[i]
+        } else if m1[s[i]] != t[i] {
+            return false
+        }
+        
+        if _, ok := m2[t[i]]; !ok {
+            m2[t[i]] = s[i]
+        } else if m2[t[i]] != s[i] {
+            return false
+        }
+    }
+    
+    return true
+}
